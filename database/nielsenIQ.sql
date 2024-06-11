63ff69f4c7f2c71310084ef7e47bc72c1a768f98
KARTHIKEYAN-SRIRAM-V <karthikeyansriramv@gmail.com>
2024-06-12 02:29:13 +0530

    COMMIT

---
+++ database/nielsenIQ.sql
@@ -0,0 +1,108 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: nielseniq
+-- ------------------------------------------------------
+-- Server version	8.4.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `product_id` varchar(255) DEFAULT NULL,
+  `relevency_score` double DEFAULT NULL,
+  `shopper_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UKat03k6o77o1rru4e6jtn4vbx7` (`product_id`),
+  KEY `FKr6rjsapu1k2qy98xbw4jtd3oh` (`shopper_id`),
+  CONSTRAINT `FKr6rjsapu1k2qy98xbw4jtd3oh` FOREIGN KEY (`shopper_id`) REFERENCES `shopper` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (3,'BB-2144746855',66.01234355,4),(4,'MD-543564697',88.552212211,4);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_meta_data`
+--
+
+DROP TABLE IF EXISTS `product_meta_data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product_meta_data` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `brand` varchar(255) DEFAULT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  `product_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `product_id_UNIQUE` (`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_meta_data`
+--
+
+LOCK TABLES `product_meta_data` WRITE;
+/*!40000 ALTER TABLE `product_meta_data` DISABLE KEYS */;
+INSERT INTO `product_meta_data` VALUES (8,'Apple','Smart-Phone','BB-2144746855'),(9,'Samsung','Smart-Phone','MD-543564697'),(10,'VW','Vehicle','MB-2093193398'),(12,'Samsung','Television','MB-2093193390');
+/*!40000 ALTER TABLE `product_meta_data` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shopper`
+--
+
+DROP TABLE IF EXISTS `shopper`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shopper` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `shopper_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UKf6edwtf6fh0wpm7aldyuosurv` (`shopper_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shopper`
+--
+
+LOCK TABLES `shopper` WRITE;
+/*!40000 ALTER TABLE `shopper` DISABLE KEYS */;
+INSERT INTO `shopper` VALUES (4,'2');
+/*!40000 ALTER TABLE `shopper` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-12  2:28:55
